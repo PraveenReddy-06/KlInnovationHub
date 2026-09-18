82e8eac44ac59c94894478cace04a75f6f1fb8e7
Praveen Maramreddy <2400032662@kluniversity.in>
2026-09-18 21:00:23 +0530

    Add public analytics DTO

---
+++ HubBackEnd/src/main/java/com/klu/dto/PublicAnalyticsDto.java
@@ -0,0 +1,29 @@
+package com.klu.dto;
+
+import java.util.Map;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PublicAnalyticsDto {
+
+    private long totalStudents;
+
+    private long totalProjects;
+    private long soloProjects;
+    private long groupProjects;
+
+    private long approvedProjects;
+    private long pendingProjects;
+    private long rejectedProjects;
+
+    private long activeReviewers;
+
+    private long totalProjectLikes;
+
+    private Map<String, Long> projectsByDomain;
+}
